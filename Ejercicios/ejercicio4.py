1d7906a272e9c1a679e4316f7bc9acb3737a0e82
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2023-09-27 19:10:39 +0200

    cmt

---
+++ Ejercicios/ejercicio4.py
@@ -0,0 +1,9 @@
+# Importar el módulo SymPy
+import sympy as sym
+from sympy import symbols
+from sympy.plotting import plot
+f1 = "10-x"
+f2 = "1+x"
+f3 = "4"
+x = symbols('x')
+plot(f1, f2, f3, (x, 15, 0))
---
+++ Ejercicios/ejercicio5.py
@@ -0,0 +1,10 @@
+# Modelo 1b
+# Importar el módulo SymPy
+import sympy as sym
+from sympy import symbols
+from sympy.plotting import plot
+f1 = "10-x"
+f2 = "x-1"
+f3 = "4"
+x = symbols('x')
+plot(f1, f2, f3, (x, 15, 0))
