06dafbcc8c3324a2cce827588c3e5d17c6d78a02
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2023-09-28 17:04:08 +0200

    cmt

--- Ejercicios/ejercicio5.py
+++ Ejercicios/ejercicio5.py
@@ -1,3 +1,5 @@
+'''
+
 # Modelo 1b
 # Importar el módulo SymPy 
 import sympy as sym
@@ -32,3 +34,22 @@ f3 = "x-1" #>=
 f4 = "4" # <=
 x = symbols('x')
 plot(f1, f2, f3, f4, (x, 15, 0))
+
+'''
+
+# Modelo 1
+# Importar el módulo SymPy
+import sympy as sym
+from sympy import symbols
+from sympy.plotting import plot
+f1 = "10-x" #<=
+f2 = "1+x" #<=
+#y=4
+f3 = "4"  #<=
+Z1 = "(16-x)/2"
+Z2 = "(14-x)/2"  #esta es la solucion
+Z3 = "(13-x)/2"
+x = symbols('x')
+plot(f1, f2, f3, Z1, Z2, Z3, (x, 15, 0))
+
+# sol Z=14 x1=6 x2=4
